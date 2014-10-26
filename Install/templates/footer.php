9b3ea1079c8a6afb0f9912d2ff9fdf3a9a6c12dc
szho42 <zhongsj52@gmail.com>
2014-10-25 20:28:34 -0400

    Remove wemall from Install

--- Install/templates/footer.php
+++ Install/templates/footer.php
@@ -1,1 +1,1 @@
-<div class="footer">  2013 - 2014 <a href="http://www.inuoer.com" target="_blank">WeMall微信商城管理系统</a>  </div>
+<div class="footer">  2013 - 2014 <a href="http://www.inuoer.com" target="_blank">WeShops商店平台管理系统</a>  </div>
